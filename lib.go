89d5b59e405a3626db05773820a4e4e6c1cf790a
Jong Yoon Lee <yoonlee95@gmail.com>
2017-11-30 20:47:48 +0000

    got lib.go to be compatable with a jetson

--- lib.go
+++ lib.go
@@ -5,7 +5,8 @@ package tensorrt
 // #cgo LDFLAGS: -lnvinfer -lnvcaffe_parser -lcudart -L${SRCDIR} -lstdc++
 // #cgo CXXFLAGS: -std=c++11  -O3 -Wall -g
 // #cgo CXXFLAGS: -Wno-sign-compare -Wno-unused-function
-// #cgo CXXFLAGS: -I/usr/local/cuda/include -I/usr/include/x86_64-linux-gnu -I/opt/frameworks/tensorrt/include
+// #cgo linux, amd64 CXXFLAGS: -I/usr/local/cuda/include -I/usr/include/x86_64-linux-gnu -I/opt/frameworks/tensorrt/include
+// #cgo arm64 CXXFLAGS: -I/usr/local/cuda/include -I/usr/include/aarch64-linux-gnu -I/opt/frameworks/tensorrt/include
 // #cgo CXXFLAGS: -I${SRCDIR}/cbits -I${SRCDIR}/cbits/util -I${SRCDIR}/cuda
-// #cgo linux,amd64 LDFLAGS: -L/usr/local/cuda/lib64 -L/opt/frameworks/tensorrt/lib -lcudart
+// #cgo LDFLAGS: -L/usr/local/cuda/lib64 -L/opt/frameworks/tensorrt/lib -lcudart
 import "C"
